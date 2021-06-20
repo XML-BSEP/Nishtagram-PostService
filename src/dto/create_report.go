0e1e6eb59aa03698e3495040198e7e0ce95b42e3
jelena-vlajkov <vlajkovj31@gmail.com>
2021-06-20 03:42:05 +0200

    :sparkles: Connected reports with frontend

--- src/dto/create_report.go
+++ src/dto/create_report.go
@@ -1,8 +1,8 @@
 package dto
 
 type CreateReportDTO struct {
-	PostId string `json:"post_id" validate:"required"`
-	ReportedBy string `json:"reported_by" validate:"required"`
-	ReportType string `json:"report_type" validate:"required"`
-	ReportedPostBy string `json:"reported_post_by" validate:"required"`
+	PostId string `json:"postId" validate:"required"`
+	ReportedBy string `json:"reportedBy" validate:"required"`
+	ReportType string `json:"reportType" validate:"required"`
+	ReportedPostBy string `json:"reportedPostBy" validate:"required"`
 }
